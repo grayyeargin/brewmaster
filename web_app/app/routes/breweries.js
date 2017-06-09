0ebfdc84d6a05508a439a205815c20266c2d9d4a
grayyeargin <grayyeargin@gmail.com>
2017-06-09 01:07:33 -0400

    Add news articles to brewery landing page

--- web_app/app/routes/breweries.js
+++ web_app/app/routes/breweries.js
@@ -3,7 +3,8 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 	model() {
 		return Ember.RSVP.hash({
-    	breweries: this.get('store').findAll('brewery', {})
+    	breweries: this.get('store').findAll('brewery', {}),
+    	news: Ember.$.getJSON('http://localhost:8080/api/news')
     })
   }
 });
