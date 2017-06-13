946bd2386a8ba823df1e9edbb55c861538bf8f96
grayyeargin <grayyeargin@gmail.com>
2017-06-13 12:50:54 -0400

    Add navigation component and initial markup

--- web_app/tests/acceptance/breweries-test.js
+++ web_app/tests/acceptance/breweries-test.js
@@ -4,9 +4,27 @@ import moduleForAcceptance from 'web-app/tests/helpers/module-for-acceptance';
 moduleForAcceptance('Acceptance | breweries');
 
 test('visiting /breweries', function(assert) {
-  visit('/');
+  visit('/breweries');
 
   andThen(function() {
-    assert.equal(currentURL(), '/');
+    assert.equal(currentURL(), '/breweries', 'should use breweries url');
   });
 });
+
+test('should list breweries', function(assert) {
+  visit('/breweries');
+
+  andThen(function() {
+    assert.equal(find('.top-table tbody tr').length, 10, 'should see 10 breweries listed');
+  });
+});
+
+test('should shows news articles', function(assert) {
+  visit('/breweries');
+
+  andThen(function() {
+    assert.equal(find('.news-row .card').length, 3, 'should see 3 news articles');
+  });
+});
+
+
